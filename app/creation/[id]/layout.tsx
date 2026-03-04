0893f7c29c1c27254bc3ad9475902fe187f7c69e
tegrakmd <tegrakamanda@gmail.com>
2026-03-04 03:45:49 +0100

    delete a Vulnability vercel{npx fix-react2shell-next}

--- app/creation/[id]/layout.tsx
+++ app/creation/[id]/layout.tsx
@@ -1,11 +1,11 @@
 import { ReactNode } from "react";
 
-export default function LayoutCreation({children}:{children:ReactNode}){
+export default function LayoutCreation({ children }: { children: ReactNode }) {
     return (
-        <section className=" my-20">
+        <section className="my-20">
             <div className="max-w-4xl mx-auto px-4 md:px-6 ">
                 {children}
             </div>
         </section>
-        )
+    )
 }

c8aef2c63e5a283b4a14eed15a56acf091b59bbf
tegrakmd <tegrakamanda@gmail.com>
2026-03-04 04:01:58 +0100

    let commit and i pnpm

--- app/creation/[id]/layout.tsx
+++ app/creation/[id]/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function LayoutCreation({ children }: { children: ReactNode }) {
     return (
         <section className="my-20">
-            <div className="max-w-4xl mx-auto px-4 md:px-6 ">
+            <div className="max-w-4xl mx-auto px-4 md:px-6">
                 {children}
             </div>
         </section>
